b6ca42c6f419e60f10809d7177a4ed055111537a
lisa <li.mw.lisa@gmail.com>
2016-09-17 13:48:16 -0700

    updated package.json, working axios

--- app/containers/ProductDetails.tsx
+++ app/containers/ProductDetails.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react'
+import axios from '../axios'
 
 export class ProductDetails extends React.Component<any, any> {
-  constructer() {
+  constructor() {
+    super()
     this.state = {
+      productDetails: {}
     }
   }
 
+  componentDidMount() {
+    axios.get('/products/10089623') // should be getting the SKU from props
+      .then(function(res: any) {
+          console.log(res);
+          console.log("success");
+          this.setState({productDetails: res.data.data});
+      })
+      .catch(function() {
+          console.error("error");
+      })
+  }
+
   render() {
+
 		return (
 			<div className="container">
         <h1>Product Details</h1>
